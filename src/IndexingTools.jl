d5c40c1178623996adc1ba32f2465d22b7c4ce9b
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2022-06-23 19:07:01 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/IndexingTools.jl
@@ -0,0 +1,5 @@
+module IndexingTools
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using IndexingTools
+using Test
+
+@testset "IndexingTools.jl" begin
+    # Write your tests here.
+end
